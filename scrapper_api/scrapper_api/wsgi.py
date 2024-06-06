88012a332e3ce3fa289494aaea37977129a33519
iabarca1 <iabarca@symnetics.cl>
2024-06-06 09:34:29 -0400

    heroku commit

--- scrapper_api/scrapper_api/wsgi.py
+++ scrapper_api/scrapper_api/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapper_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraper_project.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
